5b8a27d6e9bbd33531ca71b90aec2b3353b4f8c7
Pavlo <pkavulych@gmail.com>
2021-07-03 18:09:19 +0300

    'mini fix layout'

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,18 +5,20 @@ import {NavLink} from "react-router-dom";
 import kage from '../../assets/imeges/kage.png'
 
 let Header = React.memo(({auth, photoUsers, deleteAuth}) => {
+
+
   return (
     <div className={s.Header}>
       <img src={logo} alt={''}
-           className={s.Logo}
-      />
-      {(auth.isAuth) ? <div className={s.bar}>
+           className={s.Logo}/>
+      {(auth.isAuth)
+        ?<div className={s.bar}>
           <p className={s.name}>
             {auth.profileUserData.fullName}
           </p>
           {(photoUsers)
             ? <img className={s.ava} src={photoUsers} alt=''/>
-            : <img className={s.ava} src={kage} alt=''/> }
+            : <img className={s.ava} src={kage} alt=''/>}
           <button style={{background: "red"}}
                   type="button"
                   className={s.button}
